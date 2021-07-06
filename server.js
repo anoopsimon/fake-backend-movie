65b85b367e1cb16eb52db0de44c4568d4b9a0e0e
Anoop Simon <anoop.simon@adudev.com>
2021-07-06 16:02:43 +1000

    added json server auth

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth')
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+app.use(auth)
 server.use(middlewares);
 server.use(router);
 
