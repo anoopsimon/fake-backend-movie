5075b191c595d387886caae90f48fa649f80723a
Anoop Simon <anoop.simon@adudev.com>
2021-07-07 11:23:04 +1000

    re wrote server js

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ const jsonServer = require('json-server');
 const auth = require('json-server-auth')
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({ noCors: true });
+const middlewares = jsonServer.defaults();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
-
-server.db = router.db
 
 server.use(auth)
 server.use(middlewares);
-server.use(router);
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
+);
+server.options('*', cors());
 
-server.listen(port);
+server.use(router);
+server.listen(port, () => {
+    console.log('JSON Server is running');
+});
