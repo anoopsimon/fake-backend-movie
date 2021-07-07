669357dd40048e7f5b504c4afa483341eeac698f
Anoop Simon <anoop.simon@adudev.com>
2021-07-07 13:49:22 +1000

    added cors and middleware

--- server.js
+++ server.js
@@ -26,15 +26,25 @@
 
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
- const cors = require('cors');
+const cors = require('cors');
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
 const port = process.env.PORT || 3000;
+const middlewares = jsonServer.defaults();
 
 // /!\ Bind the router db to the app
 app.db = router.db
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
+);
 app.options('*', cors());
 // You must apply the auth middleware before the router
 app.use(auth)
+app.use(middlewares)
 app.use(router)
 app.listen(port)
